bc833d63e5182076b3c3a3a2e9218efa0cbb1e92
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-06-03 20:41:44 +0200

    Fix lesson retrieving error

--- LessonPage.js
+++ LessonPage.js
@@ -1,39 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import axios from "axios";
 
 const LessonPage = ({ route }) => {
-  const { courseId, accessToken } = route.params; // Retrieve the courseId and accessToken from route params
+  const { courseId, accessToken } = route.params;
   const [lessonData, setLessonData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetchLessonData();
-  }, []);
-
-  const fetchLessonData = async () => {
+  const fetchData = async () => {
     try {
       const response = await axios.get(
         `https://myselena.org/wp-json/learnpress/v1/lessons/${courseId}`,
         {
           headers: {
             Authorization: `Bearer ${accessToken}`
+          },
+          params: {
+            context: "view"
           }
         }
       );
       setLessonData(response.data);
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.error(err); // This will print the error to the console
+      setError(err.toString());
+    } finally {
+      setLoading(false);
     }
   };
 
+  fetchData();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.courseName}>{courseId}</Text>{" "}
-      {/* Display the courseId */}
-      {lessonData ? (
-        <Text>{JSON.stringify(lessonData, null, 2)}</Text>
-      ) : (
+      <Text style={styles.courseName}>Course: {courseId}</Text>
+      {loading ? (
         <Text>Loading lesson data...</Text>
+      ) : error ? (
+        <Text>{error}</Text>
+      ) : (
+        <Text>{JSON.stringify(lessonData, null, 2)}</Text>
       )}
     </View>
   );
