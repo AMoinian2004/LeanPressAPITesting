5882b24f003b8f0d38d9a28454955523ca538535
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-06-04 21:31:47 +0200

    Update LessonDetail.js

--- LessonDetail.js
+++ LessonDetail.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import axios from "axios";
 
 const LessonDetail = ({ route }) => {
-  const { lesson } = route.params;
+  const { lessonId, accessToken } = route.params;
+  const [lesson, setLesson] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchLesson = async () => {
+      try {
+        const response = await axios.get(
+          `https://myselena.org/wp-json/learnpress/v1/lessons/${lessonId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+            params: {
+              context: "view",
+            },
+          }
+        );
+        setLesson(response.data);
+      } catch (err) {
+        console.error(err);
+        setError(err.toString());
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchLesson();
+  }, [accessToken, lessonId]);
+
+  if (loading) {
+    return <Text>Loading lesson data...</Text>;
+  }
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
 
   return (
     <View style={styles.container}>
@@ -15,13 +53,13 @@ const LessonDetail = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10
+    padding: 10,
   },
   lessonName: {
     fontSize: 18,
     fontWeight: "bold",
-    marginBottom: 10
-  }
+    marginBottom: 10,
+  },
 });
 
 export default LessonDetail;
