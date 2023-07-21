bff594c5a5204d708639cb741293730a767bdde2
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-07-21 01:38:04 +0100

    Update LessonDetail.js

--- LessonDetail.js
+++ LessonDetail.js
@@ -1,37 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import axios from "axios";
 
-const LessonDetail = ({ route }) => {
-  const { url } = route.params;
+const LessonDetail = ({ navigation, route }) => {
+  const { lessonId, accessToken } = route.params;
+  const [lesson, setLesson] = useState({});
   const [pdfLink, setPdfLink] = useState("");
+  const [pdf, setPdf] = useState();
 
   useEffect(() => {
-    const extractPdfLink = () => {
-      const regex = /href="(.*?)"/;
-      const match = regex.exec(url);
-      if (match && match[1]) {
-        const link = match[1];
-        setPdfLink(link);
-        console.log("PDF Link:", link);
+    const extractDownloadLink = (content) => {
+      const start = content.indexOf('href="') + 6;
+      const end = content.indexOf('">download<') - 2;
+      const string = content.substring(start, end);
+
+      return string;
+    };
+
+    const getLesson = async () => {
+      try {
+        const response = await axios.get(
+          `https://myselena.org/wp-json/learnpress/v1/lessons/${lessonId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          }
+        );
+
+        setPdfLink(extractDownloadLink(response.data.content));
+        setLesson(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
       }
     };
 
-    extractPdfLink();
+    getLesson();
   }, []);
 
+  const getPdf = async () => {
+    const result = await WebBrowser.openBrowserAsync(pdfLink);
+    setPdf(result);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>PDF Link: {pdfLink}</Text>
+    <View>
+      <Text>{lessonId}</Text>
+      <Text>{pdf}</Text>
+      <Text>{pdfLink}</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
 
 export default LessonDetail;
